ae66f17dd2b92f7d32a93d7416f8a3a837a90eb4
Anews01 <anews01@naver.com>
2023-03-02 14:18:36 +0900

    [명훈] 개인작업 정리
    모든카테고리 불러오기 수정

---
+++ gitTestProject/src/main/java/kr/or/iei/fripCategory/controller/AllCategoryServlet.java
@@ -0,0 +1,57 @@
+package kr.or.iei.fripCategory.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.iei.frip.service.FripService;
+import kr.or.iei.frip.vo.Frip;
+
+/**
+ * Servlet implementation class AllCategoryServlet
+ */
+@WebServlet(name = "AllCategory", urlPatterns = { "/allCategory.do" })
+public class AllCategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AllCategoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//인코딩
+		request.setCharacterEncoding("utf-8");
+		//값추출
+		//비즈니스로직
+		FripService service = new FripService();
+		ArrayList<Frip> list = service.selectAllFrip();
+		//결과처리
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/category/categoryAll.jsp");
+		request.setAttribute("list", list);
+		
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
