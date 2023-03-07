e34e66dad63f34cc5d4e8d3183964f38b9af9d53
simminseobe <alstjq047687@gmail.com>
2023-03-07 16:19:45 +0900

    admiPage.do access denied Create
    Create

--- gitTestProject/src/main/java/kr/or/iei/admin/controller/AllMemberServlet.java
+++ gitTestProject/src/main/java/kr/or/iei/admin/controller/AllMemberServlet.java
@@ -8,9 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.or.iei.admin.service.adminService;
 import kr.or.iei.admin.vo.MemberPageDate;
+import kr.or.iei.member.vo.Member;
 
 @WebServlet(name = "AllMember", urlPatterns = { "/allMember.do" })
 public class AllMemberServlet extends HttpServlet {
@@ -24,6 +26,36 @@ public class AllMemberServlet extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
+
+		HttpSession session = request.getSession();
+
+		Member member = (Member) session.getAttribute("m");
+
+		if (member != null) { // 로그인이 된경우
+			if (member.getMemberLevel() != 1) { // 관리자가 아니면
+				RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+
+				request.setAttribute("title", "접근제한");
+				request.setAttribute("msg", "관리자인지 확인해주세요.");
+				request.setAttribute("icon", "warning");
+				request.setAttribute("loc", "/");
+
+				view.forward(request, response);
+
+				return;
+			}
+		} else { // 로그인이 안된 경우
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+
+			request.setAttribute("title", "접근제한");
+			request.setAttribute("msg", "로그인 후 접속하세요.");
+			request.setAttribute("icon", "info");
+			request.setAttribute("loc", "/");
+
+			view.forward(request, response);
+
+			return; // 아래 코드들 실행x
+		}
 
 		int reqPage = Integer.parseInt(request.getParameter("reqPage"));
 
